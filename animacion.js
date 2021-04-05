d4c5f0e6cdb50fe6ea0eefe72ecc3063d23512a1
fajardo2204 <juanfa_2204@hotmail.es>
2021-04-04 22:40:03 -0500

    Actualizacion Seccion Animacion
    Se empieza a usar js para crear una animacion donde se transladara la claqueta

---
+++ animacion.js
@@ -0,0 +1,23 @@
+let cerrar=document.querySelectorAll(".close")[0];
+let abrir=document.querySelectorAll(".cta")[0];
+let modal=document.querySelectorAll(".modal")[0];
+let modalc=document.querySelectorAll(".modal-container")[0];
+
+abrir.addEventListener("click", function(e) {
+    e.preventDefault();
+    modalc.style.opacity="1";
+    modalc.style.visibility="visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function(){
+    modal.classList.toggle("modal-close");
+    setTimeout(function(){
+        modalC.style.opacity="0";
+        modalC.style.visibility="hidden";
+    },1000)
+});
+
+window.addEventListener("click", function(){
+
+});
