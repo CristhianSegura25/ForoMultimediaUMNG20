e2668fe6889aa1591799ab8f210c99366b4884a1
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-04-06 19:05:15 -0500

    cambios no subidos

--- programacion.js
+++ programacion.js
@@ -5,34 +5,35 @@
 // 	}
 // })
 
-let cerrarp=document.querySelectorAll(".closep")[0];
-let abrirp=document.querySelectorAll(".recuadro3")[0];
-let modalp=document.querySelectorAll(".modalp")[0];
-let modalCp=document.querySelectorAll(".modalcontainerp")[0];
+let cerrarpro=document.querySelectorAll(".closepro")[0];
+let abrirpro=document.querySelectorAll(".logoiconito")[0];
+let modalpro=document.querySelectorAll(".modalpro")[0];
+let modalCpro=document.querySelectorAll(".modalcontainerpro")[0];
 
-abrirp.addEventListener("click",function(e){
-	e.preventDefault();
-	modalCp.style.opacity="1";
-	modalCp.style.visibility="visible";
-	modalp.classList.toggle("modal-closep");
+abrirpro.addEventListener("click",function(b){
+	b.preventDefault();
+	modalCpro.style.opacity="1";
+	modalCpro.style.visibility="visible";
+	modalpro.classList.toggle("modal-closepro");
 });
 
-cerrarp.addEventListener("click",function(){
-	modalp.classList.toggle("modal-closep");
+cerrarpro.addEventListener("click",function(){
+	modalpro.classList.toggle("modal-closepro");
+
 	setTimeout(function(){
-	modalCp.style.visibility="hidden";
-	modalCp.style.opacity="0";
+	modalCpro.style.visibility="hidden";
+	modalCpro.style.opacity="0";
 	},1000)
 });
 
-window.addEventListener("click",function(e){
-	console.log(e.target);
-	if(e.target==modalCp){
-		modalp.classList.toggle("modal-closep");
+window.addEventListener("click",function(b){
+	console.log(b.target);
+	if(b.target==modalCpro){
+		modalpro.classList.toggle("modal-closepro");
 
 	setTimeout(function(){
-	modalCp.style.visibility="hidden";
-	modalCp.style.opacity="0";
+	modalCpro.style.visibility="hidden";
+	modalCpro.style.opacity="0";
 	},1000);
 	}
 });
