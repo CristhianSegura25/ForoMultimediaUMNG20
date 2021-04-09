83ae83d831b7748a80a0797cb1c18ac0089ee11f
fajardo2204 <juanfa_2204@hotmail.es>
2021-04-08 21:57:59 -0500

    Creacion seccion acerca de la carrera
    se crea el modal y se adjuntan elementos de prueba

--- home.js
+++ home.js
@@ -3,6 +3,11 @@ let abrir=document.querySelectorAll(".logoinicio")[0];
 let modal=document.querySelectorAll(".modal")[0];
 let modalC=document.querySelectorAll(".modalcontainer")[0];
 
+let cerrar1=document.querySelectorAll(".close1")[0];
+let abrir1=document.querySelectorAll(".cta")[0];
+let modal1=document.querySelectorAll(".modal1")[0];
+let modalC1=document.querySelectorAll(".modal-container1")[0];
+
 abrir.addEventListener("click",function(e){
 	e.preventDefault();
 	modalC.style.opacity="1";
@@ -26,6 +31,33 @@ window.addEventListener("click",function(e){
 	setTimeout(function(){
 	modalC.style.visibility="hidden";
 	modalC.style.opacity="0";
+	},1000);
+	}
+});
+
+abrir1.addEventListener("click",function(e){
+	e.preventDefault();
+	modalC1.style.opacity="1";
+	modalC1.style.visibility="visible";
+	modal1.classList.toggle("modal-close1");
+});
+
+cerrar1.addEventListener("click",function(){
+	modal1.classList.toggle("modal-close1");
+	setTimeout(function(){
+	modalC1.style.visibility="hidden";
+	modalC1.style.opacity="0";
+	},1000)
+});
+
+window1.addEventListener("click",function(e){
+	console.log(e.target);
+	if(e.target==modalC1){
+		modal1.classList.toggle("modal-close1");
+
+	setTimeout(function(){
+	modalC1.style.visibility="hidden";
+	modalC1.style.opacity="0";
 	},1000);
 	}
 });
