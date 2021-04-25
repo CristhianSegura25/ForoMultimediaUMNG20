e4e5360f537ee26b020e4605d8124d304089e005
fajardo2204 <juanfa_2204@hotmail.es>
2021-04-24 20:46:13 -0500

    ACTUALIZACION XD

--- animacion.js
+++ animacion.js
@@ -3,6 +3,11 @@ let abrir=document.querySelectorAll(".cta")[0];
 let modal=document.querySelectorAll(".modal")[0];
 let modalc=document.querySelectorAll(".modal-container")[0];
 
+let cerrarmodelado1=document.querySelectorAll(".closemodelado1")[0];
+let abrirmodelado1=document.querySelectorAll(".logoinfo")[0];
+let modalmodelado1=document.querySelectorAll(".modalmodelado1")[0];
+let modalCmodelado1=document.querySelectorAll(".modalcontainermodelado1")[0];
+
 abrir.addEventListener("click", function(e) {
     e.preventDefault();
     modalc.style.opacity="1";
@@ -28,4 +33,49 @@ window.addEventListener("click",function(e){
 	modalc.style.opacity="0";
 	},550);
 	}
+});
+
+abrirmodelado1.addEventListener("click",function(b){
+	b.preventDefault();
+	modalCmodelado1.style.opacity="1";
+	modalCmodelado1.style.visibility="visible";
+	modalmodelado1.classList.toggle("modal-closemodelado1");
+});
+cerrarmodelado1.addEventListener("click",function(){
+	modalmodelado1.classList.toggle("modal-closemodelado1");
+
+	setTimeout(function(){
+	modalCmodelado1.style.visibility="hidden";
+	modalCmodelado1.style.opacity="0";
+	},1000)
+});
+
+//
+
+window.addEventListener("click",function(b){
+	console.log(b.target);
+	if(b.target==modalCmodelado1){
+		modalmodelado1.classList.toggle("modal-closemodelado1");
+
+	setTimeout(function(){
+	modalCmodelado1.style.visibility="hidden";
+	modalCmodelado1.style.opacity="0";
+	},1000);
+	}
+});
+
+$('#showhide').on('click', function(e){
+    $("#info_modelo").toggle();    
+});
+
+$('#showhide_2').on('click', function(e){
+    $("#info_modelo_2").toggle();    
+});
+
+$('#showhide_3').on('click', function(e){
+    $("#info_modelo_3").toggle();    
+});
+
+$('#showhide_4').on('click', function(e){
+    $("#info_modelo_4").toggle();    
 });
