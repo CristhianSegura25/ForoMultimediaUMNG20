851dff4c35e25bbcc156411e12734f068536dd28
CristhianSegura25 <78514017+CristhianSegura25@users.noreply.github.com>
2021-04-30 22:11:50 -0500

    a

---
+++ render 2.js
@@ -0,0 +1,32 @@
+
+let cerrarrender1=document.querySelectorAll(".closerender1")[0];
+let abrirrender1=document.querySelectorAll(".logoinfo")[0];
+let modalrender1=document.querySelectorAll(".modalrender1")[0];
+let modalCrender1=document.querySelectorAll(".modalcontainerrender1")[0];
+
+abrirrender1.addEventListener("click",function(b){
+	b.preventDefault();
+	modalCrender1.style.opacity="1";
+	modalCrender1.style.visibility="visible";
+	modalrender1.classList.toggle("modal-closerender1");
+});
+cerrarrender1.addEventListener("click",function(){
+	modalrender1.classList.toggle("modal-closerender1");
+
+	setTimeout(function(){
+	modalCrender1.style.visibility="hidden";
+	modalCrender1.style.opacity="0";
+	},1000)
+});
+
+window.addEventListener("click",function(b){
+	console.log(b.target);
+	if(b.target==modalCrender1){
+		modalrender1.classList.toggle("modal-closerender1");
+
+	setTimeout(function(){
+	modalCrender1.style.visibility="hidden";
+	modalCrender1.style.opacity="0";
+	},1000);
+	}
+});
