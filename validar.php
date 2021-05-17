e0cd2558fd745d7b235c218cba1f5e06bbd5d0ab
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-16 19:35:42 -0500

    Update validar.php

--- validar.php
+++ validar.php
@@ -1,31 +1,24 @@
 
-<?php
+ <?php
 	session_start();
 	require_once "conexionadb.php";
 
 	$nombreingreso=$_POST['nombreingreso'];
 	$passwordingreso=$_POST['passwordingreso'];
-
+	session_start();
 	$_SESSION['nombreingreso']=$nombreingreso;
-
 	$consulta="SELECT*FROM Lista_Usuarios where ((usuario='$nombreingreso') or (correo='$nombreingreso'))AND contrasena='$passwordingreso'";
-	
-
-	$resultado=mysqli_query($conn,$consulta);
+	$resultado=mysqli_query($conexion,$consulta);
 	$filas=mysqli_num_rows($resultado);
-
 	if($filas){
-	
 		header("location:home.php");
-
-	}
+		}
 	else{
 		include("login.php");
-?>
-			<p class="error"><b>ERROR EN LA AUTENTIFICACION</b></p>
-
-<?php
+		?>
+		<p class="error"><b>ERROR EN LA AUTENTIFICACION</b></p>
+		<?php
+		}
 	mysqli_free_result($resultado);
-	mysqli_close($conn);
-
-?>
+	mysqli_close($conexion);
+	?>
