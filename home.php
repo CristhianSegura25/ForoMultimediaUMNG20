f82ba5ea548d64099f13dc6b06bb401b42e21675
CristhianSegura25 <78514017+CristhianSegura25@users.noreply.github.com>
2021-05-21 17:33:52 -0500

    Update home.php

--- home.php
+++ home.php
@@ -9,23 +9,23 @@
 <body>
     <div class="Container">
         <div class="Zona_Botones">
-           <a href="SeccionAnimacion.html"><img id="img_1" onmouseover="setNewImage_1()" onmouseout="setOldImage_1()" class="logoicono" src="images/Ilustraciones/Logo_Animacion.png" alt="logoanimacion"/></a>
+           <a href="SeccionAnimacion.html"><img id="img_1" onmouseover="setNewImage_1()" onmouseout="setOldImage_1()" class="logoicono img-fluid" src="images/Ilustraciones/Logo_Animacion.png" alt="logoanimacion"/></a>
         </div>
 
         <div class="Zona_Botones">
-            <a href="seccioncortosguiones.html" ><img id="img_2" onmouseover="setNewImage_2()" onmouseout="setOldImage_2()" class="logoicono" src="images/Ilustraciones/Logo_Guiones.png" alt="logocortosguiones"/></a>
+            <a href="seccioncortosguiones.html" ><img id="img_2" onmouseover="setNewImage_2()" onmouseout="setOldImage_2()" class="logoicono img-fluid" src="images/Ilustraciones/Logo_Guiones.png" alt="logocortosguiones"/></a>
         </div>
 
         <div class="Zona_Botones">
-            <a href="SeccionModelado.html" ><img id="img_3" onmouseover="setNewImage_3()" onmouseout="setOldImage_3()" class="logoicono" class="logoicono" src="images/Ilustraciones/Logo_Modelado.png" alt="logomodelado"/></a>
+            <a href="SeccionModelado.html" ><img id="img_3" onmouseover="setNewImage_3()" onmouseout="setOldImage_3()" class="logoicono img-fluid" src="images/Ilustraciones/Logo_Modelado.png" alt="logomodelado"/></a>
         </div>
         
         <div class="Zona_Botones">
-            <a href="seccionprogramacion.html" ><img id="img_4" onmouseover="setNewImage_4()" onmouseout="setOldImage_4()" class="logoicono" src="images/Ilustraciones/Logo_Programacion.png" alt="logoprogramacion"/></a>
+            <a href="seccionprogramacion.html" ><img id="img_4" onmouseover="setNewImage_4()" onmouseout="setOldImage_4()" class="logoicono img-fluid" src="images/Ilustraciones/Logo_Programacion.png" alt="logoprogramacion"/></a>
         </div>
 
         <div class="Zona_Botones">
-            <a href="seccion_render.html" ><img id="img_5" onmouseover="setNewImage_5()" onmouseout="setOldImage_5()" class="logoicono" src="images/Ilustraciones/Logo_Render.png" alt="logorender"/></a>
+            <a href="seccion_render.html" ><img id="img_5" onmouseover="setNewImage_5()" onmouseout="setOldImage_5()" class="logoicono img-fluid" src="images/Ilustraciones/Logo_Render.png" alt="logorender"/></a>
         </div>
     </div>
     <footer>
