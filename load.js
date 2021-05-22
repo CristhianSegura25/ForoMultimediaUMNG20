0ddca4061f26b6168cef1f380c1d565019b7ff33
CristhianSegura25 <78514017+CristhianSegura25@users.noreply.github.com>
2021-05-21 19:32:01 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,11 +1,9 @@
 function delayRedirect(){
-    document.getElementById('delayMsg').innerHTML = 'Please wait you ll be redirected after <span id="countDown">5</span> seconds....';
     var count = 5;
     setInterval(function(){
         count--;
-        document.getElementById('countDown').innerHTML = count;
         if (count == 0) {
-            window.location = 'https://www.google.com'; 
+            window.location = 'home.php'; 
         }
     },1000);
 }
