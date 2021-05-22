95e5471d0677c28468cfaeb3976535fabd258159
CristhianSegura25 <78514017+CristhianSegura25@users.noreply.github.com>
2021-05-21 19:25:38 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 function delayRedirect(){
-    document.getElementById('delayMsg').innerHTML = 'Please wait you'll be redirected after <span id="countDown">5</span> seconds....';
+    document.getElementById('delayMsg').innerHTML = 'Please wait you ll be redirected after <span id="countDown">5</span> seconds....';
     var count = 5;
     setInterval(function(){
         count--;
