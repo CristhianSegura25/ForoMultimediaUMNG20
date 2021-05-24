db1fa337b1585508d673a93389e96c9f039c7967
CristhianSegura25 <78514017+CristhianSegura25@users.noreply.github.com>
2021-05-24 16:24:42 -0500

    Update home.php

--- home.php
+++ home.php
@@ -11,8 +11,8 @@
 </head>
 <body>
 
-    <div class="row">
-    <div class="row Contain_logos col-12 col-md-12 justify-content-around">
+    
+    <div class="row Container col-12 col-md-12 justify-content-around">
         <div class="Zona_Animacion col-10 col-md-2">
             <button class="Boton_Animacion" onclick="window.location='SeccionAnimacion.html'" title="Ir a la Sección de Animacion 2D y 3D"><img id="img_1" onmouseover="setNewImage_1()" onmouseout="setOldImage_1()" class="logoicono img-fluid" src="images/Ilustraciones/Logo_Animacion.png" alt="logoanimacion"/></button>
         </div>
@@ -195,7 +195,7 @@
                 </script>
             </div>
     </footer>
-    </div>
+
 
     <script src="homexd.js"></script>
     <script src="home2.js"></script>
