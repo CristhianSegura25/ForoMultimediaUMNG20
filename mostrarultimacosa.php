e4ae74aeea06d3ddd4a43cf2805c171853e754bc
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-23 23:53:56 -0500

    a

--- mostrarultimacosa.php
+++ mostrarultimacosa.php
@@ -1,19 +1,20 @@
 <?php
 require_once "conexionadb.php";
 
-$sql="SELECT * FROM Lista_Preguntas order by id asc limit 1";
-while ($row=mysqli_fetch_array($sql)) { ?>
-	<div class="barra-superior">
-		<p> <a><?php echo $row["tipo"]?></a> </p>
-		<a href="#"><img class="closeforo" src="images/boton cerrar.png" alt="cerrar"></a>
-	</div>
-	<div class="contenidomodal">
-		<div class="titulomodal">
-			<a><?php echo $row["titulo"]?></a>
+$sql=$conn->query("SELECT * FROM Lista_Preguntas order by id asc limit 1");
+if(mysqli_num_rows($sql)>0){ 
+	while ($row=mysqli_fetch_array($sql)) { ?>
+		<div class="barra-superior">
+			<p> <a><?php echo $row["tipo"]?></a> </p>
+			<a href="#"><img class="closeforo" src="images/boton cerrar.png" alt="cerrar"></a>
 		</div>
-		<div class="preguntamodal">
-			<a><?php echo $row["pregunta"]?></a>
+		<div class="contenidomodal">
+			<div class="titulomodal">
+				<a><?php echo $row["titulo"]?></a>
+			</div>
+			<div class="preguntamodal">
+				<a><?php echo $row["pregunta"]?></a>
+			</div>
 		</div>
-	</div>
-
-<?php } ?>
+	<?php}
+ } ?>
