fdde06cc1faed969bbb1ed5e3f93eeb61eb44359
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-25 01:26:43 -0500

    a

--- mostrarultimacosaprogramacion.php
+++ mostrarultimacosaprogramacion.php
@@ -4,6 +4,7 @@ require_once "conexionadb.php";
 $sql=$conn->query("SELECT * FROM Lista_Preguntas WHERE tipo='Programacion' order by id asc limit 1");
 if(mysqli_num_rows($sql)>0){ 
 	while ($row=mysqli_fetch_array($sql)) { ?>
+		<P>Nombre usuario: <?php echo $row["autor"] ?></P>
 		<p>Fecha y hora de publicacion: <?php echo $row["timestamp"] ?></p>
 		<a href="#" class="ctaforo1"> <?php echo $row["titulo"]?> </a>
                         <div class="modalcontainerforo1">
