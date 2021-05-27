628096670fd252333e4bf621235481086a339b57
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-26 23:21:05 -0500

    Update grespuestaanimacion.php

--- grespuestaanimacion.php
+++ grespuestaanimacion.php
@@ -2,7 +2,6 @@
 require_once "conexionadb.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-	if(empty(trim($_POST["respuesta"]))){
 		$respuesta=$_POST['respuesta'];
 
 
@@ -11,7 +10,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		$sql="UPDATE `Lista_Preguntas` SET `respuesta` = '$respuesta' WHERE tipo='Animacion' order by id desc limit 1";
 		if($conn->query($sql)===false){
 			die($conn->error);
-		}
 	}
 }
 ?>
