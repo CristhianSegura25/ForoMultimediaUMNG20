c62e61cf53bf0b759efd5e64120f53c9fa14fd0a
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-26 22:47:49 -0500

    Update gpregunta.php

--- gpregunta.php
+++ gpregunta.php
@@ -7,10 +7,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		$titulo=$_POST['titulo'];
 		$pregunta=$_POST['pregunta'];
 		$autor=$_POST['autor'];
+		$respuesta='Aun no han respondido esta pregunta';
 
 		// $pincript=password_hash($contrasena,PASSWORD_DEFAULT);
 
-		$sql="INSERT INTO Lista_Preguntas(tipo,titulo,pregunta,autor,respuesta) VALUES ('$tipo','$titulo','$pregunta','$autor','Aun no han respondido esta pregunta')";
+		$sql="INSERT INTO Lista_Preguntas(tipo,titulo,pregunta,autor,respuesta) VALUES ('$tipo','$titulo','$pregunta','$autor','$respuesta')";
 		if($conn->query($sql)===false){			die($conn->error);
 		}
 	}
