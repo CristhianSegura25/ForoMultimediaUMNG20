0125e370d5cdb621a98c8f35923f8e3fdaa98813
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-26 22:44:29 -0500

    a

---
+++ gpregunta.php
@@ -0,0 +1,19 @@
+<?php
+require_once "conexionadb.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	if(!empty(trim($_POST["tipo"])) &&!empty(trim($_POST["titulo"])) && !empty(trim($_POST["pregunta"]))&& !empty(trim($_POST["autor"]))){
+		$tipo=$_POST['tipo'];
+		$titulo=$_POST['titulo'];
+		$pregunta=$_POST['pregunta'];
+		$autor=$_POST['autor'];
+
+		// $pincript=password_hash($contrasena,PASSWORD_DEFAULT);
+
+		$sql="INSERT INTO Lista_Preguntas(tipo,titulo,pregunta,autor,respuesta) VALUES ('$tipo','$titulo','$pregunta','$autor','Aun no han respondido esta pregunta')";
+		if($conn->query($sql)===false){			die($conn->error);
+		}
+	}
+}
+
+?>
