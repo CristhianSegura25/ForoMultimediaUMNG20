6e4f81b4b1bda31aa4d77f302376925e7ffecabd
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-26 21:40:06 -0500

    Login sin registro

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta charset="utf-8" />
     <title>FORO 20 AÑOS MULTIMEDIA</title>
-    <link rel="stylesheet"  href="stylelogin.css" />
+    <link rel="stylesheet"  href="stylelog.css" />
 </head>
 <body>
     <header>
@@ -16,15 +16,18 @@
     	<img class="logo20login" src="https://s3.gifyu.com/images/Login-20-anos-nuevo-720-2.gif" alt="logo20años">
     </div>
     <div class="IngresoDatos">
+
     	<form  action="validar.php" method="POST">
     		<p><input type="text" name="nombreingreso" placeholder="Usuario/Correo"></p><br>
     		<p><input type="password" name="passwordingreso" placeholder="Contraseña"></p>
     		<div class="botoneslogin">
-    			<p><input type="reset" value="GOOGLE"></p><br>
+    			
     			<p><input type="submit" value="INGRESAR"></p>
     		</div>
     	</form>
-
+        <form method="get" action="home.php">
+            <button type="submit"><a>ENTRAR SIN REGISTRO<a></button>
+        </form>
         <div class="registro">
             <button class="botonregistro" onclick="window.location.href='registro.php'">REGISTRARSE</button>
         </div>
