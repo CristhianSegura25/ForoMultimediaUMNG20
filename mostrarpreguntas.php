8be88ed116084792d2c65e75c156ef165ea1d86f
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2021-05-27 23:15:53 -0500

    A

--- mostrarpreguntas.php
+++ mostrarpreguntas.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href ="stylemostrar.css"/>
+        <link rel="stylesheet" href ="stylemostra.css"/>
         <link rel="preconnect" href="https://fonts.gstatic.com/" >
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
         <title>Mostrar Base de Datos</title>
