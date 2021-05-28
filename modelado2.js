a79abdf3a8be6e897b557de223a1bf4069edfcf3
CristhianSegura25 <78514017+CristhianSegura25@users.noreply.github.com>
2021-05-28 10:34:44 -0500

    aaaaaaaaa

---
+++ modelado2.js
@@ -0,0 +1,79 @@
+function setNewImage_1(){
+	document.getElementById("img1").src = "https://i.ibb.co/HCrz06d/Primer-Modelo-2.gif"
+}
+
+function setOldImage_1(){
+	document.getElementById("img1").src = "images/Modelos/Primer_Modelo.png"
+}
+
+function setNewImage_2(){
+	document.getElementById("img2").src = "https://i.ibb.co/f1Sf2hr/Segundo-Modelo.gif"
+}
+
+function setOldImage_2(){
+	document.getElementById("img2").src = "images/Modelos/Segundo_Modelo.png"
+}
+
+function setNewImage_3(){
+	document.getElementById("img3").src = "https://i.ibb.co/JmP15jX/Tercer-Modelo.gif"
+}
+
+function setOldImage_3(){
+	document.getElementById("img3").src = "images/Modelos/Tercer_Modelo.png"
+}
+
+function setNewImage_4(){
+	document.getElementById("img4").src = "https://i.ibb.co/B2XTdtS/Cuarto-Modelo.gif"
+}
+
+function setOldImage_4(){
+	document.getElementById("img4").src = "images/Modelos/Cuarto_Modelo.png"
+}
+
+function setNewImage_5(){
+	document.getElementById("img5").src = "https://i.ibb.co/G3Y9JXz/Quinto-Modelo.gif"
+}
+
+function setOldImage_5(){
+	document.getElementById("img5").src = "images/Modelos/Quinto_Modelo.png"
+}
+
+function setNewImage_6(){
+	document.getElementById("img6").src = "https://i.ibb.co/sV51ptS/Sexto-Modelo.gif"
+}
+
+function setOldImage_6(){
+	document.getElementById("img6").src = "images/Modelos/Sexto_Modelo.png"
+}
+
+function setNewImage_7(){
+	document.getElementById("img7").src = "https://i.ibb.co/XXPtvXn/Septimo-Modelo.gif"
+}
+
+function setOldImage_7(){
+	document.getElementById("img7").src = "images/Modelos/Septimo_Modelo.png"
+}
+
+function setNewImage_8(){
+	document.getElementById("img8").src = "https://i.ibb.co/55wGnRD/Octavo-Modelo.gif"
+}
+
+function setOldImage_8(){
+	document.getElementById("img8").src = "images/Modelos/Octavo_Modelo.png"
+}
+
+function setNewImage_foro(){
+	document.getElementById("foro").src = "https://i.ibb.co/vQVqyYy/Logo-Foro.gif"
+}
+
+function setOldImage_foro(){
+	document.getElementById("foro").src = "images/Ilustraciones/Logo_Foro.png"
+}
+
+function setNewImage_logo_20(){
+	document.getElementById("logo_20").src = "https://i.ibb.co/djdfZ2p/Logo-20.gif"
+}
+
+function setOldImage_logo_20(){
+	document.getElementById("logo_20").src = "images/Ilustraciones/Logo_20.png"
+}
